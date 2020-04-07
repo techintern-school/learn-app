5a562dbbe9630f725e4704c624024a0c1b903b60
“Geoff <geoff616@gmail.com>
2020-04-06 17:19:47 -1000

    setup circle ci

--- backend/server.js
+++ backend/server.js
@@ -5,3 +5,11 @@ const port = 3000
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+/* TODO create routes:
+
+1. student registration
+2. get curriculum for student
+3. process submission
+
+*/

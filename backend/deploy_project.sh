f7f4bcd89339d7f65fabbc25674e419cec96a269
“Geoff <geoff616@gmail.com>
2020-04-06 19:34:58 -1000

    update deploy to kill node servers

--- backend/deploy_project.sh
+++ backend/deploy_project.sh
@@ -1,5 +1,16 @@
+echo "cloning repo and installing server"
 cd curapp
 git pull
 cd backend
 npm i
+echo "killing previous node processes"
+ps aux | grep " node " | grep -v grep
+nodepids=$(ps aux | grep " node " | grep -v grep | cut -c10-15)
+for nodepid in ${nodepids[@]}
+do
+echo "Stopping PID :"$nodepid
+kill -9 $nodepid
+done
+echo "running node server"
 nohup node server.js > output.log &
+exit 0

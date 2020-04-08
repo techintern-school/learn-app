8ada5bbde8058d5445cceee2b784a4bd30726640
“Geoff <geoff616@gmail.com>
2020-04-07 17:46:37 -1000

    bigger test of netlify diffing...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hello World!
+        A bigger change to the frontend to confim if netlify diffing will catch this :/
       </header>
     </div>
   );

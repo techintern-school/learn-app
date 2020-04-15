794550e09cea5ead96390fa4cde6189580f32e96
“Geoff <geoff616@gmail.com>
2020-04-15 09:07:03 -1000

    add make-project helper

--- curicculum/cutils.js
+++ curicculum/cutils.js
@@ -1,6 +1,7 @@
 const { Command } = require('commander');
 var fs = require('fs');
 var ncp = require('ncp').ncp;
+const shortid = require('shortid');
 
 const configPath = "./config.json"
 const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
@@ -36,18 +37,38 @@ program.command('make-version')
             });
         }
         // write updated config to JSON sile
-        fs.writeFileSync(configPath, JSON.stringify(Object.assign({}, config, {currentCurriculumVersion: newCurriculumVersion}), null, 4))
+        fs.writeFileSync(configPath, JSON.stringify(Object.assign({}, config, { currentCurriculumVersion: newCurriculumVersion }), null, 4))
     });
 
 program.command('make-project <name>')
     .alias('mp')
     .description('generate the next project with the specified name')
     .action((name) => {
-        /* 
-          TODO generate new project 
-          - Figure out how many files there are already in the currentCurriculiumVersion folder
-          - Create a new file with the nane {index}-{name}.yml with default content
-        */
+        // TODO maybe enforce name uniqueness and check to make sure in file-friendly-format
+        const projectsDirectory = `${__dirname}${config.cDirectory}/${config.currentCurriculumVersion}`
+        fs.readdir(projectsDirectory, (err, files) => {
+            let nextProjectIndex;
+            if (files.length) {
+                // get indexes of current projects
+                const currentProjects = files.map(fileName => fileName.split('-')[0])
+                console.log(currentProjects)
+                const lastProjectIndex = currentProjects.sort().reverse()[0]
+                console.log(lastProjectIndex)
+                nextProjectIndex = parseInt(lastProjectIndex) + 1;
+            } else {
+                nextProjectIndex = 0
+            }
+            const newProjectFileName = `${nextProjectIndex}-${name}.yml`
+            const newProjectPath = `${projectsDirectory}/${newProjectFileName}`
+            const templateFile = `${__dirname}${config.templateDirectory}/project.yml`
+            const template = fs.readFileSync(templateFile, 'utf8');
+            const macrosReplacedTemplate = template
+                .replace('%%NAME%%', name)
+                .replace('%%INDEX%%', nextProjectIndex)
+                .replace('%%ID%%', shortid.generate())
+                
+            fs.writeFileSync(newProjectPath, macrosReplacedTemplate);       
+        });
     });
 
 program.command('make-section <type')

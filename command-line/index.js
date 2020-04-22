e3269e5db4a493c6ff80cef2d14ab104bc2d9862
“Geoff <geoff616@gmail.com>
2020-04-21 15:47:46 -1000

    update db on challenge completed

--- command-line/index.js
+++ command-line/index.js
@@ -2,6 +2,49 @@ const { Command } = require('commander');
 const readlineSync = require('readline-sync');
 const fs = require('fs');
 const homedir = require('os').homedir();
+const {execSync} = require('child_process');
+const firebase = require("firebase/app");
+
+require("firebase/firestore");
+
+var firebaseConfig = {
+    apiKey: "AIzaSyB-I5W47oNXCcaToXY2UIBnRZYSHV6PLEk",
+    authDomain: "valiant-realm-274223.firebaseapp.com",
+    databaseURL: "https://valiant-realm-274223.firebaseio.com",
+    projectId: "valiant-realm-274223",
+    storageBucket: "valiant-realm-274223.appspot.com",
+    messagingSenderId: "937291685569",
+    appId: "1:937291685569:web:b0d68b09c3c88ddae27e40",
+    measurementId: "G-BG65RP0TJK"
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firestore = firebase.firestore();
+
+function updateCompletedSections(uid, completedSection, projectID) {
+    return new Promise((resolve, reject) => {
+        const projectDoc = firestore.collection("users").doc(uid).collection('projects').doc(projectID)
+
+        projectDoc.get()
+            .then((doc) => {
+                const previouslyCompleted = doc.data().completedSections || []        
+                const completedSections = [...previouslyCompleted, completedSection]
+                projectDoc.set({completedSections}, {merge: true})
+                    .then(function () {
+                        console.log("Document successfully written!");
+                        resolve()
+                    })
+                    .catch(function (error) {
+                        console.error("Error writing document: ", error);
+                        reject()
+                    });
+            })
+     });
+    
+    
+}
 
 const program = new Command();
 program.version('0.0.1');
@@ -19,14 +62,37 @@ program.command('setup')
     });
 
 program.command('run')
-    .description('run the program assocaited with the challenge in the current directory')
+    .description('run the program associated with the challenge in the current directory')
     .action(() => {
         // get the config file for the user from the configFile directory
+        const userConfig = JSON.parse(fs.readFileSync(configFile, 'utf8'))
         // get the config file for the challenge from the current directory 
-          // TODO define schema - shell command to execute, ???
+        // TODO define schema - shell command to execute, ???
+        const challengeConfig = JSON.parse(fs.readFileSync(`${__dirname}/.tisP.json`, 'utf8'));
+
         // run the evaluation code 
+        let output;
+        let error;
+        try {
+            output = execSync(challengeConfig.exec);
+        } catch(e) {
+            error = e;
+            console.log(e)
+        }
+        console.log(output && output.hasOwnProperty('toString') ? output.toString() : "")
         // submit update to server
+        if (error === undefined) {
+            // update completed sections
+            updateCompletedSections(userConfig.userID, challengeConfig.challengeID, challengeConfig.projectID)
+                .then(() => process.exit())
+                .catch(() => process.exit())
+        } else {
+            // update challenge attempt
+
+        }
+        
     });
+
 
 
 

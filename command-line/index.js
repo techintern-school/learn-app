53e6c4ec23c0c84094904fb3991d7235d2c0e026
“Geoff <geoff616@gmail.com>
2020-04-27 14:04:52 -1000

    temp

--- command-line/index.js
+++ command-line/index.js
@@ -68,6 +68,7 @@ program.command('setup')
     .description('run this command to link your ti.s account to the command line application')
     .action(() => {
         userID = readlineSync.question('What is your user techIntern.school user ID? You should copy and paste this from the web application... ');
+        // TODO validate userID against DB
         githubID = readlineSync.question('What is your user github user name? You should copy and paste this from the github website... ');
         const config = {
             userID, githubID
